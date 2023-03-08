f7bc3e488d3a50272b345d87b0a127cfc12b43de
Markus Leben <markus_leben@mac.com>
2023-03-08 14:15:48 -0800

    Static React elements done

--- 1-glossary/client/src/index.jsx
+++ 1-glossary/client/src/index.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import { render } from "react-dom";
+import AddForm from './components/AddForm.jsx';
+import EntryList from './components/EntryList.jsx';
+import FilterBox from './components/FilterBox.jsx';
 
 render(
   <div>
     <p>Hello, World!</p>
+    <AddForm/>
+    <EntryList/>
+    <FilterBox/>
   </div>,
   document.getElementById("root")
 );
